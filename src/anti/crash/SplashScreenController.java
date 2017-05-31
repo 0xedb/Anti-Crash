45d8360ca0d4605a586ad6e86e61fe9cca41271d
bruno <bruno@theBashShell>
2017-05-31 21:32:29 +0000

    Added splash screen fxml and controller

---
+++ src/anti/crash/SplashScreenController.java
@@ -0,0 +1,23 @@
+
+package anti.crash;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author bruno
+ */
+public class SplashScreenController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
