3bd022137e257628f790c810c5d890078ec3e201
bruno <bruno@theBashShell>
2017-05-31 20:31:28 +0000

    Started Project

---
+++ src/anti/crash/AntiCrashController.java
@@ -0,0 +1,19 @@
+
+package anti.crash;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author bruno
+ */
+public class AntiCrashController implements Initializable {
+        
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
