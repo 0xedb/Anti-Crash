c48877594be6c379ea95d5fe0d3b6ff852dddd41
bruno <bruno@theBashShell>
2017-06-01 00:04:09 +0000

    About to push what i've done so far

--- src/anti/crash/AntiCrashController.java
+++ src/anti/crash/AntiCrashController.java
@@ -1,4 +1,3 @@
-
 package anti.crash;
 
 import java.io.IOException;
@@ -7,6 +6,7 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -21,43 +21,43 @@ public class AntiCrashController implements Initializable {
 
     @FXML
     private AnchorPane landingPage;
-    
+
     public static boolean hasFaded = false;
-        
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        if (!hasFaded)
+        if (!hasFaded) {
             startSplashScreen();
-    }    
-    
-    
+        }
+    }
+
     private void startSplashScreen() {
         try {
             AnchorPane splashPane = FXMLLoader.load(getClass().getResource("SplashScreen.fxml"));
             landingPage.getChildren().setAll(splashPane);
-            
+
             FadeTransition fadeIn = new FadeTransition(Duration.seconds(7), splashPane);
             fadeIn.setFromValue(0);
             fadeIn.setToValue(1);
             fadeIn.setCycleCount(1);
-            
+
             FadeTransition fadeOut = new FadeTransition(Duration.seconds(3), splashPane);
             fadeOut.setFromValue(1);
             fadeOut.setToValue(0);
             fadeOut.setCycleCount(1);
-            
+
             fadeIn.play();
             fadeIn.setOnFinished(e -> fadeOut.play());
-            fadeOut.setOnFinished(e -> {
+            fadeOut.setOnFinished((ActionEvent e) -> {
                 try {
-                    AnchorPane programHome = FXMLLoader.load(getClass().getResource("AntiCrash.fxml"));
+                    AnchorPane programHome = FXMLLoader.load(AntiCrashController.this.getClass().getResource("AntiCrash.fxml"));
                     landingPage.getChildren().setAll(programHome);
                 } catch (IOException ex) {
                     Logger.getLogger(AntiCrashController.class.getName()).log(Level.SEVERE, null, ex);
                 }
             });
-            
-            hasFaded = true;
+
+            hasFaded = true;        // prevent infinite transition loop
         } catch (IOException ex) {
             Logger.getLogger(AntiCrash.class.getName()).log(Level.SEVERE, null, ex);
         }
