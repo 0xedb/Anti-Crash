46473bacd36444db80c895c510daa8a693637c19
Bruno Edoh <edohbruno@gmail.com>
2017-07-16 15:08:21 +0000

    Finish control page side pane interface design

--- src/anticlash/ControlPaneController.java
+++ src/anticlash/ControlPaneController.java
@@ -1,12 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
 package anticlash;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTimePicker;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -16,12 +18,88 @@ import javafx.fxml.Initializable;
  */
 public class ControlPaneController implements Initializable {
 
+    @FXML
+    private JFXCheckBox daily;
+    @FXML
+    private JFXCheckBox weekday;
+    @FXML
+    private JFXCheckBox weekend;
+    @FXML
+    private JFXCheckBox mon;
+    @FXML
+    private JFXCheckBox tue;
+    @FXML
+    private JFXCheckBox wed;
+    @FXML
+    private JFXCheckBox thur;
+    @FXML
+    private JFXCheckBox fri;
+    @FXML
+    private JFXCheckBox sat;
+    @FXML
+    private JFXCheckBox sun;
+    
+    ArrayList<JFXCheckBox> chb = new ArrayList<>();
+    @FXML
+    private JFXTimePicker startTime;
+    @FXML
+    private JFXTimePicker endTime;
+    @FXML
+    private JFXButton add;
+    
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        chb.add(mon); chb.add(tue); chb.add(wed); chb.add(thur);
+        chb.add(fri); chb.add(sat); chb.add(sun);
+        
+        
+         
     }    
+
+    @FXML
+    private void selectAll(ActionEvent event) {
+        if(daily.isSelected())
+            disableAll(chb);
+        else
+            enableAll(chb);
+    }
+    
+    private void disableAll(ArrayList<JFXCheckBox> a) {
+        for(JFXCheckBox b : a) {
+            b.setDisable(true);
+            b.setSelected(false);
+            weekday.setDisable(true);
+            weekend.setDisable(true);
+            
+            weekday.setSelected(false);
+            weekend.setSelected(false);
+        }
+    }
+    
+    private void enableAll(ArrayList<JFXCheckBox> a) {
+        for(JFXCheckBox b : a) 
+            b.setDisable(false);
+        weekday.setDisable(false);
+            weekend.setDisable(false);
+    }
+
+    @FXML
+    private void selectWeekday(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void selectWeekend(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void add(ActionEvent event) {
+        System.out.println(startTime.getValue());
+    }
     
 }
--- src/anticlash/LandingPageController.java
+++ src/anticlash/LandingPageController.java
@@ -1,11 +1,19 @@
  
 package anticlash;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
@@ -17,10 +25,20 @@ public class LandingPageController implements Initializable {
     private AnchorPane sidePane;
     @FXML
     private AnchorPane background;
+    @FXML
+    private JFXButton next;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
+    }
+
+    @FXML
+    private void next(ActionEvent event) throws IOException {
+        Parent secondPage = FXMLLoader.load(getClass().getResource("ControlPane.fxml"));
+        Scene scene = new Scene(secondPage);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
     }
 
 }
