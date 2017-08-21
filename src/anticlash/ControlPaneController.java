61f2b18dd1e27e6bb799f65246f286704d99e0c6
Bruno Edoh <edohbruno@gmail.com>
2017-08-21 10:35:53 +0000

    Implemented daily, weekday, and weekend selectors

--- src/anticlash/ControlPaneController.java
+++ src/anticlash/ControlPaneController.java
@@ -9,9 +9,12 @@ import com.jfoenix.controls.JFXToggleButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 
 /**
@@ -51,14 +54,24 @@ public class ControlPaneController implements Initializable {
     private JFXButton clash;
     private ScrollPane scroll;
     @FXML
-    private JFXListView<?> monList;
+    private JFXListView<Label> monList;
+
+    private final ObservableList<JFXCheckBox> chb = FXCollections
+            .observableArrayList(); //stores all checkboxes
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        // add all checkboxes to checkbox list
+        chb.add(mon);
+        chb.add(tue);
+        chb.add(wed);
+        chb.add(thur);
+        chb.add(fri);
+        chb.add(sat);
+        chb.add(sun);
     }
 
     @FXML
@@ -76,14 +89,52 @@ public class ControlPaneController implements Initializable {
 
     @FXML
     private void selectAll(ActionEvent event) {
+        checkeAll();
     }
 
     @FXML
     private void selectWeekday(ActionEvent event) {
+        checkeAll();
+        sat.setSelected(false);
+        sun.setSelected(false);
     }
 
     @FXML
     private void selectWeekend(ActionEvent event) {
+        uncheckAll();
+        sat.setSelected(true);
+        sun.setSelected(true);
     }
 
+    @FXML
+    private void monExpand(ActionEvent event) {
+        if (!monList.isExpanded()) {
+            monList.setExpanded(true);
+            monList.depthProperty().set(1);
+        } else {
+            monList.setExpanded(false);
+            monList.depthProperty().set(0);
+
+        }
+
+    }
+
+    private void checkeAll() {
+        for (JFXCheckBox a : chb) {
+            a.setSelected(true);
+        }
+    }
+
+    private void uncheckAll() {
+        for (JFXCheckBox a : chb) {
+            a.setSelected(false);
+        }
+    }
 }
+
+//
+//Label butt = new Label("Kofi");
+//        butt.setRotate(-180);
+//        butt.setStyle("-fx-font-weight: bold;");
+//        monList.getItems().add(butt);
+//        monList.getItems().add(new Label("Bruno"));
